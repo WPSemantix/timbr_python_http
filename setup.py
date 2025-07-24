609922aa1e07fc69c8f66ba0ddf3a55b399782a5
Dor Eliyahu <dor@timbr.ai>
2025-07-24 15:08:56 +0300

    update build process using new standard

--- setup.py
+++ setup.py
@@ -1,50 +1,5 @@
-import setuptools
+# This file is kept for backward compatibility
+# All configuration is now in pyproject.toml
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-  long_description = fh.read()
-
-setuptools.setup(
-  name='pytimbr_api',
-  version='1.0.8',
-  author='timbr',
-  author_email='contact@timbr.ai',
-  description='Timbr REST API connector',
-  long_description=long_description,
-  long_description_content_type="text/markdown",
-  url='https://github.com/WPSemantix/timbr_python_http',
-  download_url = 'https://github.com/WPSemantix/timbr_python_http/archive/refs/tags/v1.0.8.tar.gz',
-  project_urls={
-    "Bug Tracker": "https://github.com/WPSemantix/timbr_python_http/issues"
-  },
-  license='MIT',
-  packages=['pytimbr_api'],
-  install_requires=[
-    'requests>=2.32.3',
-  ],
-  package_data={},
-  keywords = [
-    'timbr',
-    'timbr-http',
-    'timbr-https',
-    'timbr-rest',
-    'timbr-api',
-    'timbr-rest-api',
-    'timbr-connector',
-    'PyTimbrRestAPI',
-    'PyTimbr',
-    'pytimbrapi',
-    'PyTimbrAPI',
-    'pytimbr_api',
-    'PyTimbr_API',
-  ],
-  classifiers=[
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-  ],
-)
+setup()
